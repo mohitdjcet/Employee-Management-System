415e54a2b423479973e6a7bb79c09e0f19575631
Mohit Kumar <mks.djcet@gmail.com>
2026-02-01 16:05:41 +0530

    Updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Sidebar } from './core/layout/sidebar/sidebar';
+import { Navbar } from './core/layout/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Sidebar,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hr-management-system');
 }
