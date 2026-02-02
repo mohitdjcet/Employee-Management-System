d439a5d49d8960ceab0a3a569583c4bcaf16a434
Mohit Kumar <mks.djcet@gmail.com>
2026-02-02 21:43:01 +0530

    Updated code

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component,signal } from '@angular/core';
+import { Component,inject,signal } from '@angular/core';
+import { Employee } from '../../services/employee';
+import { Leave } from '../../services/leave';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +9,29 @@ import { Component,signal } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  private employeeService = inject(Employee);
+  private leavesService = inject(Leave);
+
   totalEmployees = signal<number>(0);
   totalLeaves = signal<number>(0);
+
+  activeChart = signal<'employees' | 'leaves' | null>(null);
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe( data =>{
+      this.totalEmployees.set(data.length);
+    })
+
+    this.leavesService.getLeaves().subscribe( data =>{
+      this.totalLeaves.set(data.length);
+    })
+  }
+
+  showEmpChart(){
+    this.activeChart.set('employees');
+  }
+
+  showLeaveChart(){
+    this.activeChart.set('leaves');
+  }
 }
